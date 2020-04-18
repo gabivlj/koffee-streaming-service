10fe6f6a607c4a930beb4046e7384304b514b3da
gabivlj <gabitriqui@gmail.com>
2020-04-18 22:09:03 +0200

    Paseto generation of tokens, the key is a pkc8 document, so you must parse it and get the ed25519 public key for parsing tokens generated by this service

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
 use actix_cors::Cors;
 use actix_multipart::Multipart;
 use actix_web::{http, web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use chrono::offset::Utc;
+use chrono::Duration;
 use futures::{StreamExt, TryStreamExt};
 use http::StatusCode;
+use serde_json::json;
+use std::env;
 use std::fs;
 use std::io::Write;
 use std::path::PathBuf;
+
+#[cfg(feature = "v2")]
+use ring::signature::Ed25519KeyPair;
+
+use ring::signature::{self, KeyPair};
+
+// use ring::ec::curve25519::ed25519::signing::Ed25519KeyPair;
+
+use dotenv;
 
 extern crate rand;
 
@@ -13,8 +26,6 @@ use rand::distributions::Alphanumeric;
 use rand::Rng;
 
 use std::process::Command;
-
-use ring::signature::Ed25519KeyPair;
 
 extern crate paseto;
 
@@ -30,9 +41,10 @@ mod file_handling;
 // #[warn(dead_code)]
 fn validate_token(s: &str) -> Option<u64> {
     let footer = "";
-    let as_key =
-        Ed25519KeyPair::from_pkcs8("some_random_key_that_we'll_get_from_an_env".as_bytes())
-            .expect("Failed to parse keypair");
+    let as_key = ring::signature::Ed25519KeyPair::from_pkcs8(
+        "some_random_key_that_we'll_get_from_an_env".as_bytes(),
+    )
+    .expect("Failed to parse keypair");
     let verified = paseto::validate_public_token(
         &s,
         Some(footer),
@@ -48,20 +60,53 @@ fn validate_token(s: &str) -> Option<u64> {
     };
 }
 
-fn generate_token(path: &str, duration: f64) -> &str {
-    ""
+use std::num::ParseIntError;
+
+pub fn decode_hex(s: &str) -> Result<Vec<u8>, ParseIntError> {
+    (0..s.len())
+        .step_by(2)
+        .map(|i| u8::from_str_radix(&s[i..i + 2], 16))
+        .collect()
+}
+
+use std::io::prelude::*;
+fn generate_token(path: &str, duration: f64) -> Result<String, &str> {
+    /*
+         FOR FUTURE REFERENCE IF WE WANNA CREATE A NEW KEY
+          let rng = ring::rand::SystemRandom::new();
+          let pkcs8_bytes = signature::Ed25519KeyPair::generate_token(&rng).unwrap();
+          let pkcs8_bytes_more: &[u8] = pkcs8_bytes.as_ref();
+    */
+
+    // TODO store this in a global env at the beginning
+    let mut f = std::fs::File::open("key.txt").unwrap();
+    let mut buff: Vec<u8> = vec![];
+    f.read_to_end(&mut buff).unwrap();
+    let as_key = ring::signature::Ed25519KeyPair::from_pkcs8(buff.as_ref()).unwrap();
+    let exp = Utc::now() + Duration::days(1);
+    match paseto::tokens::PasetoBuilder::new()
+        .set_ed25519_key(as_key)
+        .set_issued_at(Some(Utc::now()))
+        .set_expiration(exp)
+        .set_claim(String::from("path"), json!(path))
+        .set_claim(String::from("duration"), json!(duration.to_string()))
+        .set_footer(String::from("xd"))
+        .build()
+    {
+        Ok(value) => Ok(value),
+        Err(_) => Err("Error generating token"),
+    }
 }
 
 async fn post_mp3(mut payload: Multipart) -> Result<HttpResponse, Error> {
     let mut filepathend = String::from("");
     let random_id = rand::thread_rng()
         .sample_iter(&Alphanumeric)
-        .take(10)
+        .take(23)
         .collect::<String>();
     fs::create_dir_all(format!("assets/media/{}/hls", random_id))
         .expect("We need to handle this error");
     fs::create_dir_all(format!("tmp/{}", random_id)).expect("We need to handle this error");
-    // that will return the filepath for this...
     while let Ok(Some(mut field)) = payload.try_next().await {
         let content_type = field.content_disposition().unwrap();
         let filename = content_type.get_filename().unwrap();
@@ -103,11 +148,24 @@ async fn post_mp3(mut payload: Multipart) -> Result<HttpResponse, Error> {
 
     match child.wait() {
         Ok(_) => {
-            println!(
-                "Duration: {}s",
-                file_handling::get_duration_from_hls(path_hls_str)
+            let token = generate_token(
+                random_id.as_str(),
+                file_handling::get_duration_from_hls(path_hls_str),
             );
-            Ok(HttpResponse::Ok().body("Success!").into())
+            match token {
+                Ok(value) => Ok(HttpResponse::Ok()
+                    .body(json!({
+                        "status": 200,
+                        "message": "Success!",
+                        "data": {
+                            "token": value
+                        }
+                    }))
+                    .into()),
+                Err(_) => Ok(HttpResponse::build(StatusCode::NOT_FOUND)
+                    .body("Error creating it!")
+                    .into_body()),
+            }
         }
         Err(_) => Ok(HttpResponse::build(StatusCode::NOT_FOUND)
             .body("Error creating it!")
@@ -152,6 +210,7 @@ async fn get_hls_file(req: HttpRequest) -> HttpResponse {
         .body(file)
 }
 
+// For testing purposes and the sake of simplicity!
 fn index() -> HttpResponse {
     let html = r#"<html>
         <head><title>Upload Test</title></head>
@@ -168,7 +227,8 @@ fn index() -> HttpResponse {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    println!("Done.");
+    dotenv::dotenv().unwrap();
+    println!("{}", generate_token("xd", 39.0).unwrap());
     HttpServer::new(|| {
         App::new()
             .wrap(Cors::new().finish())
